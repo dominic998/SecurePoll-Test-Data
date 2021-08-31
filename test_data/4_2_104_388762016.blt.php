0b59796abb97c5e9d7829bbdf7372ae71d7f7dbc
Dom Walden <drw@blackbird>
2021-08-31 15:43:38 +0100

    Test data

---
+++ test_data/4_2_104_388762016.blt.php
@@ -0,0 +1,207 @@
+<?php return array (
+  'elected' => 
+  array (
+    0 => 2,
+    1 => 4,
+  ),
+  'eliminated' => 
+  array (
+    0 => 1,
+    1 => 3,
+  ),
+  'rounds' => 
+  array (
+    0 => 
+    array (
+      'round' => 1,
+      'surplus' => 0,
+      'rankings' => 
+      array (
+        1 => 
+        array (
+          'votes' => 0,
+          'earned' => 11,
+          'total' => 11,
+        ),
+        2 => 
+        array (
+          'votes' => 0,
+          'earned' => 69,
+          'total' => 69,
+        ),
+        3 => 
+        array (
+          'votes' => 0,
+          'earned' => 11,
+          'total' => 11,
+        ),
+        4 => 
+        array (
+          'votes' => 0,
+          'earned' => 13,
+          'total' => 13,
+        ),
+      ),
+      'totalVotes' => 104,
+      'keepFactors' => 
+      array (
+        1 => 1,
+        2 => 1,
+        3 => 1,
+        4 => 1,
+      ),
+      'quota' => 34.666667666666662,
+      'elected' => 
+      array (
+      ),
+      'eliminated' => 
+      array (
+      ),
+    ),
+    1 => 
+    array (
+      'round' => 2,
+      'elected' => 
+      array (
+        0 => 2,
+      ),
+      'eliminated' => 
+      array (
+      ),
+      'surplus' => 34.333332333333338,
+      'keepFactors' => 
+      array (
+        1 => 1,
+        2 => 1,
+        3 => 1,
+        4 => 1,
+      ),
+      'quota' => 34.666667666666662,
+      'rankings' => 
+      array (
+        1 => 
+        array (
+          'votes' => 11,
+          'earned' => 0,
+          'total' => 11,
+        ),
+        2 => 
+        array (
+          'votes' => 69,
+          'earned' => 0,
+          'total' => 69,
+        ),
+        3 => 
+        array (
+          'votes' => 11,
+          'earned' => 0,
+          'total' => 11,
+        ),
+        4 => 
+        array (
+          'votes' => 13,
+          'earned' => 0,
+          'total' => 13,
+        ),
+      ),
+      'totalVotes' => 104,
+    ),
+    2 => 
+    array (
+      'round' => 3,
+      'elected' => 
+      array (
+      ),
+      'eliminated' => 
+      array (
+        0 => 1,
+        1 => 3,
+      ),
+      'surplus' => 5.4734297922705188,
+      'keepFactors' => 
+      array (
+        1 => 1,
+        2 => 0.50241547342995163,
+        3 => 1,
+        4 => 1,
+      ),
+      'quota' => 29.193237874396136,
+      'rankings' => 
+      array (
+        1 => 
+        array (
+          'votes' => 11,
+          'earned' => 3.980676212560387,
+          'total' => 14.980676212560386,
+        ),
+        2 => 
+        array (
+          'votes' => 69,
+          'earned' => -34.33333233333336,
+          'total' => 34.666667666666655,
+        ),
+        3 => 
+        array (
+          'votes' => 11,
+          'earned' => 3.9806762125603883,
+          'total' => 14.980676212560386,
+        ),
+        4 => 
+        array (
+          'votes' => 13,
+          'earned' => 9.9516905314009669,
+          'total' => 22.951690531400967,
+        ),
+      ),
+      'totalVotes' => 87.579710623188404,
+    ),
+    3 => 
+    array (
+      'round' => 4,
+      'elected' => 
+      array (
+        1 => 4,
+      ),
+      'eliminated' => 
+      array (
+      ),
+      'surplus' => 25.833506585031124,
+      'keepFactors' => 
+      array (
+        1 => 0,
+        2 => 0.42309040397675568,
+        3 => 0,
+        4 => 1,
+      ),
+      'quota' => 25.833509585031166,
+      'rankings' => 
+      array (
+        1 => 
+        array (
+          'votes' => 14.980676212560386,
+          'earned' => -14.980676212560386,
+          'total' => 0.0,
+        ),
+        2 => 
+        array (
+          'votes' => 34.666667666666655,
+          'earned' => -2.0887065604564707,
+          'total' => 32.577961106210182,
+        ),
+        3 => 
+        array (
+          'votes' => 14.980676212560386,
+          'earned' => -14.980676212560386,
+          'total' => 0.0,
+        ),
+        4 => 
+        array (
+          'votes' => 22.951690531400967,
+          'earned' => 21.970874117482321,
+          'total' => 44.922564648883274,
+        ),
+      ),
+      'totalVotes' => 77.500525755093491,
+    ),
+  ),
+);
