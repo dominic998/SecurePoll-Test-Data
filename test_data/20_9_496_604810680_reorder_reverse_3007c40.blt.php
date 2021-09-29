0cc8f5d04dcbfd372eb4687c5d3892857404ed14
Dom Walden <drw@blackbird>
2021-09-29 13:24:36 +0100

    Update latest results

---
+++ test_data/20_9_496_604810680_reorder_reverse_3007c40.blt.php
@@ -0,0 +1,528 @@
+<?php return array (
+  'elected' => 
+  array (
+    0 => 1,
+    1 => 2,
+    2 => 3,
+    3 => 4,
+    4 => 5,
+    5 => 6,
+    6 => 7,
+    7 => 8,
+  ),
+  'eliminated' => 
+  array (
+    0 => 15,
+    1 => 19,
+    2 => 18,
+    3 => 17,
+    4 => 16,
+    5 => 11,
+    6 => 14,
+    7 => 13,
+    8 => 12,
+    9 => 10,
+    10 => 9,
+    11 => 20,
+  ),
+  'rounds' => 
+  array (
+    0 => 
+    array (
+      'round' => 1,
+      'surplus' => 0,
+      'rankings' => 
+      array (
+        1 => 
+        array (
+          'votes' => 0,
+          'earned' => 50,
+          'total' => 50,
+        ),
+        2 => 
+        array (
+          'votes' => 0,
+          'earned' => 50,
+          'total' => 50,
+        ),
+        3 => 
+        array (
+          'votes' => 0,
+          'earned' => 50,
+          'total' => 50,
+        ),
+        4 => 
+        array (
+          'votes' => 0,
+          'earned' => 50,
+          'total' => 50,
+        ),
+        5 => 
+        array (
+          'votes' => 0,
+          'earned' => 50,
+          'total' => 50,
+        ),
+        6 => 
+        array (
+          'votes' => 0,
+          'earned' => 50,
+          'total' => 50,
+        ),
+        7 => 
+        array (
+          'votes' => 0,
+          'earned' => 50,
+          'total' => 50,
+        ),
+        8 => 
+        array (
+          'votes' => 0,
+          'earned' => 50,
+          'total' => 50,
+        ),
+        9 => 
+        array (
+          'votes' => 0,
+          'earned' => 8,
+          'total' => 8,
+        ),
+        10 => 
+        array (
+          'votes' => 0,
+          'earned' => 8,
+          'total' => 8,
+        ),
+        11 => 
+        array (
+          'votes' => 0,
+          'earned' => 8,
+          'total' => 8,
+        ),
+        12 => 
+        array (
+          'votes' => 0,
+          'earned' => 8,
+          'total' => 8,
+        ),
+        13 => 
+        array (
+          'votes' => 0,
+          'earned' => 8,
+          'total' => 8,
+        ),
+        14 => 
+        array (
+          'votes' => 0,
+          'earned' => 8,
+          'total' => 8,
+        ),
+        15 => 
+        array (
+          'votes' => 0,
+          'earned' => 8,
+          'total' => 8,
+        ),
+        16 => 
+        array (
+          'votes' => 0,
+          'earned' => 8,
+          'total' => 8,
+        ),
+        17 => 
+        array (
+          'votes' => 0,
+          'earned' => 8,
+          'total' => 8,
+        ),
+        18 => 
+        array (
+          'votes' => 0,
+          'earned' => 8,
+          'total' => 8,
+        ),
+        19 => 
+        array (
+          'votes' => 0,
+          'earned' => 8,
+          'total' => 8,
+        ),
+        20 => 
+        array (
+          'votes' => 0,
+          'earned' => 8,
+          'total' => 8,
+        ),
+      ),
+      'totalVotes' => 496,
+      'keepFactors' => 
+      array (
+        1 => 1,
+        2 => 1,
+        3 => 1,
+        4 => 1,
+        5 => 1,
+        6 => 1,
+        7 => 1,
+        8 => 1,
+        9 => 1,
+        10 => 1,
+        11 => 1,
+        12 => 1,
+        13 => 1,
+        14 => 1,
+        15 => 1,
+        16 => 1,
+        17 => 1,
+        18 => 1,
+        19 => 1,
+        20 => 1,
+      ),
+      'quota' => 49.600000999999999,
+      'elected' => 
+      array (
+      ),
+      'eliminated' => 
+      array (
+      ),
+    ),
+    1 => 
+    array (
+      'round' => 2,
+      'elected' => 
+      array (
+        0 => 1,
+        1 => 2,
+        2 => 3,
+        3 => 4,
+        4 => 5,
+        5 => 6,
+        6 => 7,
+        7 => 8,
+      ),
+      'eliminated' => 
+      array (
+      ),
+      'surplus' => 3.1999920000000088,
+      'keepFactors' => 
+      array (
+        1 => 1,
+        2 => 1,
+        3 => 1,
+        4 => 1,
+        5 => 1,
+        6 => 1,
+        7 => 1,
+        8 => 1,
+        9 => 1,
+        10 => 1,
+        11 => 1,
+        12 => 1,
+        13 => 1,
+        14 => 1,
+        15 => 1,
+        16 => 1,
+        17 => 1,
+        18 => 1,
+        19 => 1,
+        20 => 1,
+      ),
+      'quota' => 49.600000999999999,
+      'rankings' => 
+      array (
+        1 => 
+        array (
+          'votes' => 50,
+          'earned' => 0,
+          'total' => 50,
+        ),
+        2 => 
+        array (
+          'votes' => 50,
+          'earned' => 0,
+          'total' => 50,
+        ),
+        3 => 
+        array (
+          'votes' => 50,
+          'earned' => 0,
+          'total' => 50,
+        ),
+        4 => 
+        array (
+          'votes' => 50,
+          'earned' => 0,
+          'total' => 50,
+        ),
+        5 => 
+        array (
+          'votes' => 50,
+          'earned' => 0,
+          'total' => 50,
+        ),
+        6 => 
+        array (
+          'votes' => 50,
+          'earned' => 0,
+          'total' => 50,
+        ),
+        7 => 
+        array (
+          'votes' => 50,
+          'earned' => 0,
+          'total' => 50,
+        ),
+        8 => 
+        array (
+          'votes' => 50,
+          'earned' => 0,
+          'total' => 50,
+        ),
+        9 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        10 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        11 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        12 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        13 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        14 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        15 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        16 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        17 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        18 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        19 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        20 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+      ),
+      'totalVotes' => 496,
+    ),
+    2 => 
+    array (
+      'round' => 3,
+      'elected' => 
+      array (
+      ),
+      'eliminated' => 
+      array (
+        0 => 15,
+        1 => 19,
+        2 => 18,
+        3 => 17,
+        4 => 16,
+        5 => 11,
+        6 => 14,
+        7 => 13,
+        8 => 12,
+        9 => 10,
+        10 => 9,
+        11 => 20,
+      ),
+      'surplus' => 2.5615935960000016,
+      'keepFactors' => 
+      array (
+        1 => 0.99200001999999998,
+        2 => 0.99200001999999998,
+        3 => 0.99200001999999998,
+        4 => 0.99200001999999998,
+        5 => 0.99200001999999998,
+        6 => 0.99200001999999998,
+        7 => 0.99200001999999998,
+        8 => 0.99200001999999998,
+        9 => 1,
+        10 => 1,
+        11 => 1,
+        12 => 1,
+        13 => 1,
+        14 => 1,
+        15 => 1,
+        16 => 1,
+        17 => 1,
+        18 => 1,
+        19 => 1,
+        20 => 1,
+      ),
+      'quota' => 49.280801797999999,
+      'rankings' => 
+      array (
+        1 => 
+        array (
+          'votes' => 50,
+          'earned' => -0.3999990000000001,
+          'total' => 49.600000999999999,
+        ),
+        2 => 
+        array (
+          'votes' => 50,
+          'earned' => -0.39206301968000151,
+          'total' => 49.607936980319998,
+        ),
+        3 => 
+        array (
+          'votes' => 50,
+          'earned' => -0.3999355123161607,
+          'total' => 49.600064487683838,
+        ),
+        4 => 
+        array (
+          'votes' => 50,
+          'earned' => -0.39999849209980015,
+          'total' => 49.600001507900203,
+        ),
+        5 => 
+        array (
+          'votes' => 50,
+          'earned' => -0.39999899593680965,
+          'total' => 49.600001004063188,
+        ),
+        6 => 
+        array (
+          'votes' => 50,
+          'earned' => -0.39999899996749566,
+          'total' => 49.600001000032506,
+        ),
+        7 => 
+        array (
+          'votes' => 50,
+          'earned' => -0.39999899999974103,
+          'total' => 49.600001000000262,
+        ),
+        8 => 
+        array (
+          'votes' => 50,
+          'earned' => -0.39999899999999905,
+          'total' => 49.600000999999999,
+        ),
+        9 => 
+        array (
+          'votes' => 8,
+          'earned' => 1.6776880458616138E-17,
+          'total' => 8.0,
+        ),
+        10 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        11 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        12 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        13 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        14 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        15 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        16 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        17 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        18 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        19 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+        20 => 
+        array (
+          'votes' => 8,
+          'earned' => 0,
+          'total' => 8,
+        ),
+      ),
+      'totalVotes' => 492.80800798000001,
+    ),
+  ),
+);
